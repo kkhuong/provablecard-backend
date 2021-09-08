76d9bd28f51e53f391fef6b9f54b1c232dfa55f2
Kevin Khuong <kkhuong@ucsd.edu>
2021-09-07 17:35:00 -0700

    Use postgres

--- provablecard/settings.py
+++ provablecard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mbgw8^7%%ru00)n9lmxqdu71-w=!96o@njg)&8b9z*$p&!^xr2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['165.232.129.94']
+ALLOWED_HOSTS = ['165.232.129.94', 'localhost']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'provablecard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'provablecard',
+        'USER': 'provablecard',
+        'PASSWORD': 'provablecard',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
